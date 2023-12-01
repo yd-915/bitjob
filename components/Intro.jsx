5b6ac920a2ec9181fb3a0da7a97235529b90b2e7
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-11-30 21:26:17 -0500

    Update Intro.jsx

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -31,7 +31,7 @@ export default function Intro() {
           <h1 className='md:text-6xl text-2xl sm:text-2xl font-extrabold mb-4 text-black '>Sound Work. <span className='text-indigo-600'>For Sound money.</span> </h1>
           <p className='md:text-lg sm:text-sm text-xs mb-20 text-black-400'>Advocate your freelance skills and get paid exclusively in BTC.</p>
           <div className='bg-white flex-col mb-6 w-full md:px-4   py-4 flex sm:flex-row items-center justify-center'>
-            <BiSearchAlt className='text-2xl text-indigo-600 mx-2 hidden sm:flex' />
+            <BiSearchAlt className='text-2xl text-orange-600 mx-2 hidden sm:flex' />
             <input onChange={(e) => setSearch(e.target.value)} type="text" placeholder='Search Jobs with Job categories like marketing ...' className='xs:w-full w-3/4  h-full px-2 bg-gray-200 text-base py-3 outline-none' />
             <button onClick={handleSearch} className='px-3 py-2 my-2 sm:my-0 border border-indigo-600 rounded uppercase tracking-widest mx-4   text-white bg-indigo-600 transition-all duration-700 hover:bg-transparent font-semibold text-base hover:text-indigo-600'>Search</button>
           </div>
@@ -41,13 +41,13 @@ export default function Intro() {
               <h1 className='font-semibold text-lg'>Suggest Tag : </h1>
             </div>
             <div className='flex   items-center justify-center px-4 flex-wrap'>
-              <p className='px-2  text-gray-600'>Software</p>
-              <p className='px-2  text-gray-600'>Marketing</p>
-              <p className='px-2  text-gray-600'>IT</p>
+              <p className='px-2  text-white-600'>Software</p>
+              <p className='px-2  text-white-600'>Marketing</p>
+              <p className='px-2  text-white-600'>IT</p>
             </div>
           </div>
         </div>
-        <div className='w-3/6 my-2 h-full bg-gray-200 hidden items-center justify-center flex-col p-20 lg:flex'>
+        <div className='w-3/6 my-2 h-full bg-black-200 hidden items-center justify-center flex-col p-20 lg:flex'>
           <Image width={600} height={700} src="/try.png" alt="no-image-found" />
         </div>
       </div>
