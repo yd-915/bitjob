e40c28d09a857731ddddb59841af17a07ae8f6d8
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-11-30 21:48:38 -0500

    Update Intro.jsx

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -28,16 +28,16 @@ export default function Intro() {
     <>
       <div className='w-full  h-full flex items-center lg:justify-start py-24 justify-center flex-wrap  '>
         <div className='lg:w-3/6 w-full sm:p-2 h-full my-2 flex items-center justify-center px-4 md:items-start md:justify-start md:p-20 flex-col '>
-          <h1 className='md:text-6xl text-2xl sm:text-2xl font-extrabold mb-4 text-black '>Sound Work. <span className='text-indigo-600'>For Sound money.</span> </h1>
-          <p className='md:text-lg sm:text-sm text-xs mb-20 text-black-400'>Advocate your freelance skills and get paid exclusively in BTC.</p>
+          <h1 className='md:text-6xl text-2xl sm:text-2xl font-extrabold mb-4 text-white '>Sound Work. <span className='text-orange-600'>For Sound money.</span> </h1>
+          <p className='md:text-lg sm:text-sm text-xs mb-20 text-white-400'>Advocate your freelance skills and get paid exclusively in BTC.</p>
           <div className='bg-white flex-col mb-6 w-full md:px-4   py-4 flex sm:flex-row items-center justify-center'>
             <BiSearchAlt className='text-2xl text-orange-600 mx-2 hidden sm:flex' />
             <input onChange={(e) => setSearch(e.target.value)} type="text" placeholder='Search Jobs with Job categories like marketing ...' className='xs:w-full w-3/4  h-full px-2 bg-gray-200 text-base py-3 outline-none' />
-            <button onClick={handleSearch} className='px-3 py-2 my-2 sm:my-0 border border-indigo-600 rounded uppercase tracking-widest mx-4   text-white bg-indigo-600 transition-all duration-700 hover:bg-transparent font-semibold text-base hover:text-indigo-600'>Search</button>
+            <button onClick={handleSearch} className='px-3 py-2 my-2 sm:my-0 border border-orange-600 rounded uppercase tracking-widest mx-4   text-white bg-indigo-600 transition-all duration-700 hover:bg-transparent font-semibold text-base hover:text-indigo-600'>Search</button>
           </div>
           <div className=' w-full px-2 py-2 flex items-center justify-start flex-wrap'>
             <div className='flex items-center justify-center'>
-              <BsFillBookmarkFill className='text-indigo-600 text-xl mx-2' />
+              <BsFillBookmarkFill className='text-orange-600 text-xl mx-2' />
               <h1 className='font-semibold text-lg'>Suggest Tag : </h1>
             </div>
             <div className='flex   items-center justify-center px-4 flex-wrap'>
