968c109e0be061a1dadfecd26073a626b949ff0c
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-11-30 23:24:46 -0500

    Update postAJob.jsx

--- pages/frontend/postAJob.jsx
+++ pages/frontend/postAJob.jsx
@@ -100,46 +100,46 @@ export default function PostAJob() {
         <>
             <NavBar />
             <div className='w-full  py-20 flex items-center  justify-center flex-col'>
-                <h1 className='text-xl mt-4 uppercase tracking-widest border-b-2 border-b-indigo-600 py-2 font-semibold mb-8 md:text-2xl lg:text-4xl'>Enter Job Details</h1>
+                <h1 className='text-xl mt-4 uppercase tracking-widest border-b-2 border-b-orange-600 py-2 font-semibold mb-8 md:text-2xl lg:text-4xl'>Enter Job Details</h1>
                 <form onSubmit={handleSubmit} className="sm:w-1/2 w-full px-4 mx-4  h-full" >
                     <div className='w-full mb-4  flex flex-col items-start justify-center'>
                         <label htmlFor="title" className='mb-1 text-base font-semibold'>Title :</label>
-                        <input onChange={(e) => setFormData({ ...formData, title: e.target.value })} type="text" id='title' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter title of job' />
+                        <input onChange={(e) => setFormData({ ...formData, title: e.target.value })} type="text" id='title' className='w-full py-2 px-3 mb-2 border border-orange-600 rounded' placeholder='Enter title of job' />
                         {
                             error.title && <p className="text-sm text-red-500">{error.title}</p>
                         }
                     </div>
                     <div className='w-full mb-4  flex flex-col items-start justify-center'>
                         <label htmlFor="salary" className='mb-1 text-base font-semibold'>Salary :</label>
-                        <input onChange={(e) => setFormData({ ...formData, salary: e.target.value })} type="number" id='salary' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter Salary for this job' />
+                        <input onChange={(e) => setFormData({ ...formData, salary: e.target.value })} type="number" id='salary' className='w-full py-2 px-3 mb-2 border border-orange-600 rounded' placeholder='Enter Salary for this job' />
                         {
                             error.salary && <p className="text-sm text-red-500">{error.salary}</p>
                         }
                     </div>
                     <div className='w-full mb-4  flex flex-col items-start justify-center'>
                         <label htmlFor="email" className='mb-1 text-base font-semibold'>Email :</label>
-                        <input onChange={(e) => setFormData({ ...formData, email: e.target.value })} type="email" id='email' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter Email to be Contacted for this job' />
+                        <input onChange={(e) => setFormData({ ...formData, email: e.target.value })} type="email" id='email' className='w-full py-2 px-3 mb-2 border border-orange-600 rounded' placeholder='Enter Email to be Contacted for this job' />
                         {
                             error.email && <p className="text-sm text-red-500">{error.email}</p>
                         }
                     </div>
                     <div className='w-full mb-4  flex flex-col items-start justify-center'>
                         <label htmlFor="company" className='mb-1 text-base font-semibold'>Company :</label>
-                        <input onChange={(e) => setFormData({ ...formData, company: e.target.value })} type="text" id='company' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter Company of job' />
+                        <input onChange={(e) => setFormData({ ...formData, company: e.target.value })} type="text" id='company' className='w-full py-2 px-3 mb-2 border border-orange-600 rounded' placeholder='Enter Company of job' />
                         {
                             error.company && <p className="text-sm text-red-500">{error.company}</p>
                         }
                     </div>
                     <div className='w-full mb-4  flex flex-col items-start justify-center'>
                         <label htmlFor="description" className='mb-1 text-base font-semibold'>Description :</label>
-                        <textarea onChange={(e) => setFormData({ ...formData, description: e.target.value })} onResize={"none"} type="text" id='description' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter description of job' />
+                        <textarea onChange={(e) => setFormData({ ...formData, description: e.target.value })} onResize={"none"} type="text" id='description' className='w-full py-2 px-3 mb-2 border border-orange-600 rounded' placeholder='Enter description of job' />
                         {
                             error.description && <p className="text-sm text-red-500">{error.description}</p>
                         }
                     </div>
                     <div className='w-full mb-4  flex flex-col items-start justify-center'>
                         <label htmlFor="jobCategory" className='mb-1 text-base font-semibold'>Job Category :</label>
-                        <input onChange={(e) => setFormData({ ...formData, job_category: e.target.value })} type="text" id='jobCategory' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter Category of job' />
+                        <input onChange={(e) => setFormData({ ...formData, job_category: e.target.value })} type="text" id='jobCategory' className='w-full py-2 px-3 mb-2 border border-orange-600 rounded' placeholder='Enter Category of job' />
                         {
                             error.job_category && <p className="text-sm text-red-500">{error.job_category}</p>
                         }
@@ -159,19 +159,19 @@ export default function PostAJob() {
                     </div>
                     <div className='w-full mb-4  flex flex-col items-start justify-center'>
                         <label htmlFor="jobva" className='mb-1 text-base font-semibold'>Job Vacancy :</label>
-                        <input onChange={(e) => setFormData({ ...formData, job_vacancy: e.target.value })} type="number" id='jobva' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter Number  of Vacancies' />
+                        <input onChange={(e) => setFormData({ ...formData, job_vacancy: e.target.value })} type="number" id='jobva' className='w-full py-2 px-3 mb-2 border border-orange-600 rounded' placeholder='Enter Number  of Vacancies' />
                         {
                             error.job_vacancy && <p className="text-sm text-red-500">{error.job_vacancy}</p>
                         }
                     </div>
                     <div className='w-full mb-4  flex flex-col items-start justify-center'>
                         <label htmlFor="jobva" className='mb-1 text-base font-semibold'>Job Deadline :</label>
-                        <input onChange={(e) => setFormData({ ...formData, job_deadline: e.target.value })} type="date" id='jobva' className='w-full py-2 px-3 mb-2 border border-indigo-600 rounded' placeholder='Enter Deadline of job' />
+                        <input onChange={(e) => setFormData({ ...formData, job_deadline: e.target.value })} type="date" id='jobva' className='w-full py-2 px-3 mb-2 border border-orange-600 rounded' placeholder='Enter Deadline of job' />
                         {
                             error.job_deadline && <p className="text-sm text-red-500">{error.job_deadline}</p>
                         }
                     </div>
-                    <button type="submit" className='w-full py-2 rounded bg-indigo-600 text-white font-semibold tracking-widest'>Submit</button>
+                    <button type="submit" className='w-full py-2 rounded bg-orange-600 text-white font-semibold tracking-widest'>Submit</button>
                 </form>
             </div>
             <ToastContainer />
