766ca6bc0e243ee03c2cfaffdd9b6b8acc4bad18
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-11-30 23:19:36 -0500

    Update [id].jsx

--- pages/frontend/detailPostedJob/[id].jsx
+++ pages/frontend/detailPostedJob/[id].jsx
@@ -40,15 +40,15 @@ export default function PostedJobsDetails() {
                 isLoading ? (
 
                     <div className='bg-gray w-full h-screen flex items-center flex-col justify-center'>
-                        <InfinitySpin width='200' color="#4f46e5" />
+                        <InfinitySpin width='200' color="#E55C46" />
                         <p className='text-xs uppercase'>Loading Resources Hold Tight...</p>
                     </div>
                 ) : (
                     <>
                         <NavBar />
                         <div className='w-full  pt-20'>
-                            <div className='w-full h-20 bg-gray-50 text-indigo-600 font-bold flex items-center justify-center flex-col'>
-                                <h1 className='text-3xl'>Detail List of  Jobs Application</h1>
+                            <div className='w-full h-20 bg-gray-50 text-orange-600 font-bold flex items-center justify-center flex-col'>
+                                <h1 className='text-3xl'>Detail List of Jobs Application</h1>
                             </div>
                             <div className='w-full h-full px-4 py-4 flex  overflow-y-auto  items-start justify-center flex-wrap'>
                                 <ApplicationsDataTable application={application}  />
